3490751d3a2f843fc4c245b9b5948a36509968fa
Zolimar486 <ing.carrasquero.zolimar@gmail.com>
2022-04-10 22:35:49 -0300

    Initial commit

---
+++ Random/main.js
@@ -0,0 +1,25 @@
+const text= document.querySelector('.quote');
+const author= document.querySelector('.author');
+const btn = document.querySelector('.button');
+
+const getQuote = async() => {
+   const res= await fetch('https://type.fit/api/quotes');
+   const quotes= await res.json();
+   const num= Math.floor(Math.random()*quotes.length);
+   
+   console.log(num);
+   const items= quotes[num];
+   console.log(items);
+
+   const quote = items.text;
+   const authorName= items.author;
+   
+   text.innerText= quote;
+   author.innerText= authorName;
+}
+    
+
+btn.addEventListener('click', getQuote);
+ 
+
+getQuote()
